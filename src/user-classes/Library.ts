b805ca48cdea6a0af0f6e2359c06336a50d99f91
Adam Lynch <alynch14@students.towson.edu>
2018-11-30 00:12:02 -0500

    Added code to the Library class

--- src/user-classes/Library.ts
+++ src/user-classes/Library.ts
@@ -1,7 +1,24 @@
 import connection from './../general-server-classes/Database'
 
-class Library{
+export default class Library{
+    private name: String
+    private gameList: String[]
+
+    constructor(name: String){
+        this.name = name
+        this.gameList = Array()
+    }
+
+    public getGameList(){
+        this.gameList.fill(connection.query("SELECT ", (err) =>{
+            if(err){
+                throw err;
+            }
+            console.log("we got the list")
+        }), 0, -1)
+    }
+
 
 }
 
-export default new Library()
+// export default new Library(name)
